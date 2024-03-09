78f5f0fbe8e1c672a69b8863587356c9624610ce
Devesh08yadav <ydevesh08@gmail.com>
2024-03-09 11:25:05 +0530

    index.py modified

--- index.py
+++ index.py
@@ -1,5 +1,11 @@
+def calculateMul(a,b):
+    c=a*b
+    print("The multiplication of provided number",c)
+
+
 def calculateSum(a,b):
     c=a+b
     print("The sum of provided number is",c)
 
 calculateSum(10,20)
+calculateMul(20,5)
